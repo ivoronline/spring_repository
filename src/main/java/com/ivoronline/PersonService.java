c0ecff8f28fe47b323ecad42497ff6568ffc44ab
ivoronline <ivoronline@gmail.com>
2024-06-17 10:01:51 +0200

    Commit

--- src/main/java/com/ivoronline/PersonService.java
+++ src/main/java/com/ivoronline/PersonService.java
@@ -2,7 +2,6 @@ package com.ivoronline;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
